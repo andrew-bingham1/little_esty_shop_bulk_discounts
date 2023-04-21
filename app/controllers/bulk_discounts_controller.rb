949971418eac32ed2e1ab7c1d4af1ea40b93a6a7
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-21 13:14:10 -0600

    feat: create new and create methods for bulk discount controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,4 +7,24 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create 
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = @merchant.bulk_discounts.create(bulk_discount_params)
+    if @bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else 
+      flash.notice = "Please fill in all fields properly."
+      redirect_to new_merchant_bulk_discount_path(@merchant)
+    end
+  end
+
+  private
+  def bulk_discount_params
+    params.permit(:name, :discount, :threshold)
+  end
 end
