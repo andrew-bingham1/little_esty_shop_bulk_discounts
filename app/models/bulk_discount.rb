08ccf60b12db63761f9aa0390f0b37980af6237c
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-21 11:51:06 -0600

    fix: add validations and tests for bulk discount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,6 +1,9 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
+
+  validates_presence_of :name
+  validates_numericality_of :discount, :threshold
  
 end
 
